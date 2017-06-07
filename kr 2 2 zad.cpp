567d169b034815d175df91808793e8d175ccb188
Algurina <algurina26@gmail.com>
2017-06-07 08:22:55 +0300

    Update kr 2 2 zad.cpp

--- kr 2 2 zad.cpp
+++ kr 2 2 zad.cpp
@@ -1,63 +1,82 @@
-#include <stdlib.h>
+/*
+В бинарном файле записаны целые числа.Изменить содержимое файла,
+записав после серии полряд идущих чисел кол-во их повторений.
+*/
 #include <iostream>
 #include <fstream>
-#include <locale>
-#include <cstdio>
+#include <stdio.h>
+#include <cstdlib>
+#include <locale.h>
+
 using namespace std;
 
-int main(void)
+
+int main()
 {
-	char FileName1[20];
+	char* FileName1="work.bin";
 	int v_Count;
 	int v_Number;
 	int v_Number1;
 	int v_err;
-
-	cout << ("\Enter the name of File1: ");
-	cin >> (FileName1);
-	cout << ("\Enter the count of numbers in faile: ");
-	cin >> ("%ld%", &v_Count);
-	ofstream f1;
-	f1.open(FileName1, ios::binary);
-	for (int i = 0; i<v_Count; i++)
+		
+	cout<<"Enter the count of numbers in faile: ";
+	cin>>v_Count;
+	ofstream f1(FileName1, ios::binary);
+	for (int i=0;i<v_Count;i++)
 	{
-		printf("\Enter the count of numbers in faile: ");
-		cin >> ("%ld%", &v_Number);
-		f1.write((int*)&v_Number, sizeof(int));
+		cout<<"Enter the number: ";
+		cin>>v_Number;
+		f1.write((char*)&v_Number, sizeof(int));
+		cout<<endl;
 	}
-	f1.close();
-	ofstream f2;
-	f2.open("Temp.dat", ios::binary);
-	ifstream f11;
-	f11.open(FileName1, ios::binary);
-	f11.read((int*)&v_Number1, sizeof(int));
-	f2.write((int*)&v_Number, sizeof(int));
-	v_Count = 1;
-	while (!f11.read((int*)&v_Number, sizeof(int)).eof())
+    f1.close();
+	ofstream f2("Temp.dat", ios::binary);
+	ifstream f11(FileName1, ios::binary);
+	f11.read((char*)&v_Number1, sizeof(int));
+	f2.write((char*)&v_Number1, sizeof(int));
+	v_Count=1;
+	//while (!f11.read((char*)&v_Number, sizeof(int)).eof()) 
+	while (1)
 	{
-		f2.write((int*)&v_Number, sizeof(int));
-		if (v_Number1 == v_Number)
-		{
-			v_Count++;
-		}
+		f11.read((char*)&v_Number, sizeof(int));
+		if (f11.eof()) break;
+		if (v_Number1==v_Number)
+		{ 
+		   v_Count++;		   
+		} 
 		else
 		{
-			if v_Count>1
+			if (v_Count>1)
 			{
-				f2.write((int*)&v_Count, sizeof(int));
-				v_Count = 1;
+			  f2.write((char*)&v_Count, sizeof(int)); 
+			v_Count=1;
 			}
-			v_Number1 = v_Number;
+			v_Number1=v_Number;			
 		}
-
+		f2.write((char*)&v_Number, sizeof(int));
+  
+    }
+    
+    if (v_Count>1) 
+	{
+	 
+     f2.write((char*)&v_Count, sizeof(int));
 	}
-	f11.close;
-	f2.close;
-	v_err = remove(FaileName1);
-	if (rename("Temp.dat", FileName1) == 0)
-		cout << ("\End of work");
-	else
-		cout << ("\Error rename file");
+    
+	f11.close();
+	f2.close();
+	v_err = remove("work.bin");
+	v_err = rename("temp.dat", "work.bin");	
+	ifstream frez(FileName1, ios::binary);
+	v_Number=0;
+	//while (!frez.read((char*)&v_Number, sizeof(int)).eof())
+	while (1) 
+	{
+		frez.read((char*)&v_Number, sizeof(int));
+		if (frez.eof()) break;
+		cout<<v_Number<<endl;  
+    }
+	system("pause");
 	return 0;
-}
+	}
 
